510c9c2f454039b2656942e06ab137ca69c8e203
Jose Manuel Echeverri Palacio <palacio.90@hotmail.com>
2019-03-23 20:46:47 -0500

    Barra de progreso

--- js/myScript.js
+++ js/myScript.js
@@ -12,35 +12,43 @@ btn3 = document.getElementById("btnEstudios");
 btn4 = document.getElementById("btnDeportes");
 btn5 = document.getElementById("btnAgra");
 
+//Barra de progreso
+
+var progressBar = document.getElementById("progresBar");
 
 //Eventos
 btn1.addEventListener("click", function(){
     let btnToHide = document.getElementsByClassName("nav-link active");
     btnToHide[0].className="nav-link";
     this.className = "nav-link active";
+    progressBar.style.width="20%";
     slideCard(p1);
 });
 btn2.addEventListener("click", function(){
     let btnToHide = document.getElementsByClassName("nav-link active");
     btnToHide[0].className="nav-link";
     this.className = "nav-link active";
+    progressBar.style.width="40%";
     slideCard(p2);
 });
 btn3.addEventListener("click", function(){
     let btnToHide = document.getElementsByClassName("nav-link active");
     btnToHide[0].className="nav-link";
     this.className = "nav-link active";
+    progressBar.style.width="60%";
     slideCard(p3);
 });
 btn4.addEventListener("click", function(){
     let btnToHide = document.getElementsByClassName("nav-link active");
     btnToHide[0].className="nav-link";
+    progressBar.style.width="80%";
     this.className = "nav-link active";
     slideCard(p4);
 });
 btn5.addEventListener("click", function(){
     let btnToHide = document.getElementsByClassName("nav-link active");
     btnToHide[0].className="nav-link";
+    progressBar.style.width="100%";
     this.className = "nav-link active";
     slideCard(p5);
 });
